15729aef2918b04aaadf9717df78feb229eaf020
KimHyunIn <appkimhyunin@gmail.com>
2019-02-22 22:42:31 +0900

    52장의 카드 목록 완성, 카드번호 가져오기,
    딜러, 플레이어 카드 전달해주기 진행중.

--- CardDeck.java
+++ CardDeck.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
@@ -8,19 +9,19 @@ public class CardDeck{
     
     private static final String[] PATTERNS = {"spade", "heart", "diamond", "club"};
     ArrayList<Card> cards = new ArrayList<>();
-    public CardDeck(){
+    public void CardDecks(){
 
         for(String pattern : PATTERNS){
             for(int i=1;i<=13;i++){
                 Card card = new Card();
                 String number = this.numToStr(i);
-                card.setNumber(number);
-                card.setShape(pattern);
-                cards.add(card);
+                card.setNumber(number);//카드 번호 저장
+                card.setShape(pattern);//카드모양 저장
+                cards.add(card);       //cards 리스트에 저장
             }
         }
     }
-    private String numToStr(int number){
+    private String numToStr(int number){ //A = 1 , K = 11 , Q = 12, J = 13
         if(number == 1){
             return "A";
         }else if(number == 11){
@@ -30,26 +31,25 @@ public class CardDeck{
         }else if(number == 13){
             return "J";
         }
-        return String.valueOf(number);
+        return String.valueOf(number); //나머지 숫자들은 숫자에서 문자로
 
     }
     public ArrayList<Card> giveCard(){
         ArrayList<Card> giveCard = new ArrayList<>();
-       
         double randomNum = Math.random();
-        int rand = (int)(randomNum * 52)+1;
+        int rand = (int)(randomNum * cards.size()); //outOfBounds 에러가 발생하지않도록 하기위해 cards.size의 중 랜덤 수 하나 뽑기
         giveCard.add(cards.get(rand));
         cards.remove(rand);
         return giveCard;
     } 
-    public void shuffle(){
+    public void shuffle(){ //카드섞기
         Collections.shuffle(cards);
 
     }
 	
     
     @Override
-    public String toString() {
+    public String toString() {// 52장의 카드 출력
         StringBuilder sb = new StringBuilder();
 
         for(Card card : cards){
--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main{
     public static void main(String[] args) {
         CardDeck cardDeck = new CardDeck();
-        cardDeck.shuffle();  
-        System.out.println(cardDeck.giveCard());
-        System.out.println(cardDeck.cards.size());
-        
+        Card card = new Card();
+        cardDeck.CardDecks();
+        cardDeck.shuffle();
+        System.out.println(cardDeck.cards.get(1));
+        String shape = cardDeck.cards.get(1).getNumber(); //카드번호 가져오기
+        System.out.println(shape); 
+       
     }
 }
